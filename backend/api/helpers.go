335648580c66d4229bcff2717811663a2f1044e5
Pau Penin <paupenin@gmail.com>
2024-01-04 23:39:50 +0100

    Use browser to generate and store images based on URL

--- backend/api/helpers.go
+++ backend/api/helpers.go
@@ -2,7 +2,9 @@ package api
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"net/url"
 	"time"
 
 	"github.com/paupenin/web2image/backend/api/middleware"
@@ -32,4 +34,30 @@ func getStarttime(r *http.Request) time.Time {
 
 func getElapsedtime(r *http.Request) time.Duration {
 	return time.Since(getStarttime(r))
+}
+
+func isValidUrl(urlStr string) bool {
+	// TODO: Force valid domain or valid IP
+	// TODO: Disallow localhost and local IPs
+	// TODO: Disallow private IPs
+	// TODO: Disallow reserved IPs
+	// TODO: Disallow reserved domains
+	// TODO: Disallow reserved TLDs
+	// TODO: Disallow reserved subdomains
+	// TODO: Disallow reserved ports
+	// TODO: Disallow reserved protocols
+
+	// Parse the URL and ensure there's no error
+	parsedUrl, err := url.ParseRequestURI(urlStr)
+	if err != nil {
+		return false
+	}
+
+	// Check for the presence of a scheme (http/https) and a host
+	return parsedUrl.Scheme != "" && parsedUrl.Host != ""
+}
+
+func generateRandomString(length int) string {
+	// TODO: Generate a real random string
+	return fmt.Sprintf("%d", time.Now().UnixNano())
 }
--- backend/api/routesPrivateApi.go
+++ backend/api/routesPrivateApi.go
@@ -34,8 +34,40 @@ func (s *Server) renderHtmlHandler(w http.ResponseWriter, r *http.Request) {
 func (s *Server) renderUrlHandler(w http.ResponseWriter, r *http.Request) {
 	url := r.FormValue("url")
 
+	// Validate URL
+	if !isValidUrl(url) {
+		writeJSON(w, http.StatusBadRequest, map[string]string{
+			"error": "Invalid URL",
+		})
+		return
+	}
+
+	// Grab a browser instance from the pool
+	browser := s.browserPool.GetBrowser()
+
+	image, err := browser.RenderURL(url)
+
+	if (err != nil) || (image == nil) {
+		writeJSON(w, http.StatusInternalServerError, map[string]string{
+			"error": "Cannot render URL",
+		})
+		return
+	}
+
+	// Store image
+	imageFilename := generateRandomString(10) + ".jpg"
+	err = s.imageStore.StoreImage(imageFilename, image)
+
+	if err != nil {
+		writeJSON(w, http.StatusInternalServerError, map[string]string{
+			"error": "Cannot store image",
+		})
+		return
+	}
+
 	writeJSON(w, http.StatusOK, map[string]string{
-		"time": getElapsedtime(r).String(),
-		"url":  url,
+		"time":  getElapsedtime(r).String(),
+		"url":   url,
+		"image": s.GetURL() + s.imageStore.GetImagePath(imageFilename),
 	})
 }
